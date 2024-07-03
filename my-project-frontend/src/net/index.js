6998cdba980794dfb3f84eb5b9f3b88b5b62e4c4
MingQce <1420395899@qq.com>
2024-07-03 14:34:41 +0800

    前端:登录页面编写及axios请求封装

---
+++ my-project-frontend/src/net/index.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import {ElMessage} from "element-plus";
+
+const authItemName = "access_token";
+//默认请求失败处理
+const defaultFailure = (message, code, url) =>{
+    console.warn(`请求地址:${url}, 状态码:${code}, 错误信息:${message}`);//警告格式
+    ElMessage.warning(message)//出问题后弹窗
+}
+//默认异常处理
+const defaultError = (error) => {
+    console.error(error)
+    ElMessage.warning('发生了一些错误，请联系管理员')
+}
+//Token的获取
+function takeAccessToken(){
+    const str = localStorage.getItem(authItemName || sessionStorage.getItem(authItemName));
+    if(!str) return null;
+    const authObj = JSON.parse(str);
+}
+//Token的保存
+function storeAccessToken(token, remember, expire){
+    const authObj = {token: token, expire: expire};
+    const str = JSON.stringify(authObj);
+    //判断是否勾选了"记住我"
+    if(remember)
+        localStorage.setItem(authItemName, str);
+    else
+        sessionStorage.setItem(authItemName, str);
+    //判断是否过期
+    if(authObj.expire <= new Date()){
+        deleteAccessToken()
+        ElMessage.warning('登录状态已过期，请重新登录')
+    }
+    return authObj.token;
+}
+//Token的删除
+function deleteAccessToken(){
+    localStorage.removeItem(authItemName);
+    sessionStorage.removeItem(authItemName);
+}
+//处理内部请求
+function internalPost(url, data, header, success, failure = defaultFailure(), error = defaultError()) {
+    axios.post(url, data, {headers: header}).then( ({data})=>{
+        if(data.code === 200){
+            success(data.data)
+        }else{
+            failure(data.message, data.code, url)
+        }
+    }).catch(err => error(err))
+}
+//处理内部请求
+function internalGet(url, data, header, success, failure, error = defaultError()) {
+    axios.get(url, {headers: header}).then( ({data})=>{
+        if(data.code === 200){
+            success(data.data)
+        }else{
+            failure(data.message, data.code, url)
+        }
+    }).catch(err => error(err))
+}
+//登录请求处理
+function login(username, password, remember, success, failure = defaultFailure()) {
+    internalPost('/api/auth/login',{
+        username: username,
+        password: password,
+    },{
+        'Content-Type': 'application/x-www-form-urlencoded',    //表单发送格式
+    }, (data) => {
+        storeAccessToken(remember,data.token,data.expire)
+        ElMessage.success(`登录成功,欢迎${username}来到我们的系统`)
+        success(data)
+    },failure)
+}
+
+export {login}
--- my-project-frontend/src/views/welcome/LoginPage.vue
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -1,11 +1,34 @@
 <script setup>
-import {User} from "@element-plus/icons-vue"
-import {reactive} from "vue";
+import {User, Lock} from '@element-plus/icons-vue'
+import router from "@/router";
+import {reactive, ref} from "vue";
+import {login} from '@/net'
+
+const formRef = ref()
 const form = reactive({
-  username:'',
-  password:'',
-  remember:false,
+  username: '',
+  password: '',
+  remember: false
 })
+
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名' }
+  ],
+  password: [
+    { required: true, message: '请输入密码'}
+  ]
+}
+
+function userLogin(){
+  formRef.value.validate((valid) => {
+    if(valid){
+      login(form.username, form.password, form.remember, () => {
+
+      })
+    }
+  })
+}
 </script>
 
 <template>
@@ -17,17 +40,43 @@ const form = reactive({
   </div>
   <!-- 输入框 -->
   <div  style="margin-top: 50px">
-    <el-form v-model="form">
-      <el-form-item>
+    <el-form :model="form" :rules="rules" ref="formRef">
+      <el-form-item prop="username">
         <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名/邮箱">
           <template #prefix>
             <el-icon><User/></el-icon>
           </template>
         </el-input>
       </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
+          <template #prefix>
+            <el-icon><Lock/></el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <!-- 平分，一共24,12就是一半 -->
+      <el-row>
+        <el-col :span="12" style="text-align: left">
+          <el-form-item prop="remember">
+            <el-checkbox v-model="form.remember" label="记住我"/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12" style="text-align: right">
+          <el-link>忘记密码?</el-link>
+        </el-col>
+      </el-row>
     </el-form>
   </div>
-
+  <div style="margin-top: 40px;">
+    <el-button style="width: 270px;" type="success" plain @click="userLogin">立即登录</el-button>
+  </div>
+  <el-divider>
+    <span style="font-size: 13px;color: gray">没有账号?</span>
+  </el-divider>
+  <div>
+    <el-button style="width: 270px;" type="warning" plain>立即注册</el-button>
+  </div>
 </div>
 </template>
 
